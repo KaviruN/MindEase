6bee4aef3b5dea1361cef9bb60594bb08a0208e5
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2024-11-22 22:31:53 +0530

    remove the overlay scrolling bug

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,11 @@
 const navbar = document.querySelector('.navbar');
 
-// Add an event listener for scrolling
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
-    // Add the "scrolled" class if scrolled down
+
     navbar.classList.add('scrolled');
   } else {
-    // Remove the "scrolled" class when at the top
+
     navbar.classList.remove('scrolled');
   }
 });
@@ -17,12 +16,15 @@ function navClose() {
     navMobile.style.width = "0";
     setTimeout(() => {
         navMobile.style.display = "none";
-    }, 500); // Match the transition duration
+        document.body.classList.remove('no-scroll');
+    }, 500); 
 }
 
 function navOpen() {
     navMobile.style.display = "flex";
+    document.body.classList.add('no-scroll');
     setTimeout(() => {
         navMobile.style.width = "100%";
-    }, 10); // Small delay to trigger the transition
+    }, 10); 
+
 }
