743fe58b05dcf348d5e4b404e1ad369aefba6bf2
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2024-12-25 11:12:20 +0530

    Prepare app for deployment2

--- MentalHealthApp/settings.py
+++ MentalHealthApp/settings.py
@@ -3,7 +3,6 @@ from urllib.parse import urlparse
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-import dj_database_url
 
 
 # Quick-start development settings - unsuitable for production
