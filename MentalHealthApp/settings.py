c946bd6b6b9b42cc6e88dbb80d1a836d3c7dd7f9
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-01-16 10:34:06 +0530

    fix: update email settings for SMTP and change recipient address in contact form

--- MentalHealthApp/settings.py
+++ MentalHealthApp/settings.py
@@ -23,8 +23,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kaviruyt@gmail.com'
-EMAIL_HOST_PASSWORD = 'zeuw pdrs obue ktjc'
+EMAIL_HOST_USER = 'mindeaseorg@gmail.com'
+EMAIL_HOST_PASSWORD = 'wskv kjpq lxsn ovjl'
 
 
 
